30c776bb7fb4f7544c4d147aade03ed485887248
ildarick93 <saliakhovif@yandex.ru>
2021-09-22 12:28:02 +0300

    added users admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+    list_filter = ('username', 'email')
+    empty_value_display = '-empty-'
