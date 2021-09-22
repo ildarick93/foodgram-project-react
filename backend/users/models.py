84d85216a876f442e2ef933c5b3989a0ee3386d4
ildarick93 <saliakhovif@yandex.ru>
2021-09-22 12:24:20 +0300

    added users model and manager

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class UserRoles:
+    USER = 'user'
+    ADMIN = 'admin'
+    choices = (
+        (USER, USER),
+        (ADMIN, ADMIN),
+    )
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    role = models.CharField(
+        max_length=20,
+        choices=UserRoles.choices,
+        default=UserRoles.USER
+    )
+    USERNAME_FIELD = 'username'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'username',
+        'password',
+        'email',
+        'first_name',
+        'last_name'
+    ]
+    objects = CustomUserManager()
+
+    class Meta:
+        ordering = ['id']
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def is_admin(self):
+        return self.role == UserRoles.ADMIN
+
+    @property
+    def is_user(self):
+        return self.role == UserRoles.USER
